6c39bd09afabc4d87112db8368739cae1ce6aad5
Erin Jeter <erin@ErinsMacBookPro.attlocal.net>
2021-02-08 20:56:55 -0500

    code update

--- src/components/NASA/Nasa.js
+++ src/components/NASA/Nasa.js
@@ -9,7 +9,7 @@ let base_url = "https://api.nasa.gov/planetary/earth/assets"
 
 const Nasa = () => {
   const url = `${base_url}?lon=${long}&lat=${lat}&date=2018-02-01&&dim=0.15&api_key=${key}`;
-  const [data, setData] = useState(null);
+  const [data, setData] = useState();
 
   const initData = async () => {
     const response = await fetch(url);
@@ -18,7 +18,7 @@ const Nasa = () => {
 
     console.log(image);
 
-    setData(image);
+    setData(image.url);
   };
 
   useEffect(() => {
@@ -27,66 +27,40 @@ const Nasa = () => {
 
   return (
   <div>
-      <img src={image.url} alt="NASA satellite of your location"/>
+      <img src={data} alt="NASA satellite of your location"/>
   </div>);
 };
 
 export default Nasa;
 
-// import React, { Component } from 'react';
-// // import './Nasa.css';
 
-// export default class NASA extends Component {
-//     constructor(props){
-//         super(props);
-//         this.state = {
-//             lat: '',
-//             long: '',
-//             NASAurl: 'https://api.nasa.gov/neo/rest/v1/neo/browse',
-//             img: '',
-//             key: '6X75nJteHGPQZh0ZZ7EXhuygiSeH92RTL6rKfYbJ'
-//         };
-//     }
+//Version2
 
-//     componentWillMount() {
-//         console.log("NASA API is working on it.");
-//     }
+// export default function NasaPhoto() {
+//     const [photoData, setPhotoData] = useState(null);
 
-//     componentDidMount = (props) => {
-//         console.log('starting mount')
-//         if (navigator.geolocation) {
-//           navigator.geolocation.getCurrentPosition((position) => {
-//             this.setState({
-//                 long: (position.coords.longitude),
-//                 lat: (position.coords.latitude)
-//             })
+//     useEffect(() => {
+//         fetchPhoto();
 
-//             console.log("Latitude is :", position.coords.latitude);
-//             console.log("Longitude is :", position.coords.longitude);
-//             console.log("NASA component mounted.");
-
-//             fetch(`https://api.nasa.gov/neo/rest/v1/neo/browse?lon=86.1581&lat=9.7684&api_key=6X75nJteHGPQZh0ZZ7EXhuygiSeH92RTL6rKfYbJ`)
-//             .then(res => res.json())
-//             .then(json => {
-//                 console.log(json);
-//                 this.setState({
-//                     img: json.url
-//                 })
-//                 console.log(this.state.img);
-//             })
-//           })
+//         async function fetchPhoto() {
+//             const res = await fetch(
+//                 `${base_url}?lon=${long}&lat=${lat}&date=2018-02-01&&dim=0.15&api_key=${key}`
+//             );
+//             const data = await res.json();
+//             setPhotoData(data);
 //         }
-//       }
-    
-//     render(){
-//         return(
-//             <div id='nasaDiv'>
-//                 <h2 id='nasah2'>YOU ARE HERE</h2>
-//                 <br />
-//                 <img id='apiReturn' src={this.state.img} alt='NASA API photo'/>
-//                 <br />
-//                 <hr />
-//             </div>
-//         )
-//     }
-// // }
+//     }, []);
+
+//     if (!photoData) return <div />;
+
+//     return (
+//         <div>
+//             <img src={photoData.url} alt={photoData.title}/>
+//         </div>
+//     )
+// }
+
+
+
+
+
