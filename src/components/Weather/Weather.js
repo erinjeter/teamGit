25401930d06bf6a31439706a99b153e94aa9ccf0
traceybradley <73622421+traceybradley@users.noreply.github.com>
2021-02-08 20:53:52 -0500

    final weather api

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,57 +1,32 @@
 import React from 'react';
 import {useEffect, useState} from 'react'; //{useEffect, useState}
-// import RestaurantName from './RestaurantName';
-// import Location from '../Geolocated';
-    
+import "./Weather.css"
 const Weather = ({match}) => {
     let lat = 39.7684
     let lon = 86.1581
-    const WeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=d9c83a590f45484ed5d3808e110436db`;
+    const WeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=2dfd73a7e486ac6b59f194d965bbd0ba`;
     const [weather, setweather] = useState();
+    const [toggle, setToggle] = useState(false);
+    const [celcius, setCelcius] = useState('');
     const weatherData = async () => {
         const response = await fetch(WeatherUrl);         
         const data = await response.json();
-        console.log(data);
-        setweather(data);
+        const farenheit = data.main.temp;
+        console.log(farenheit);
+        setweather(farenheit);
+        setCelcius((farenheit - 32) * .556);
     };
     useEffect(() => {
         weatherData();
     }, [] );
-
     return (
         <div>
-           
             <h1>Indianapolis Area Weather</h1>
-            {weather?.main.temp}
-            
-            {weather?.weather[0].description}
-            
+            <div>
+                <button onClick= {() => setToggle(!toggle)}>SUBMIT</button>
+                <p>{toggle ? weather : celcius}</p>
+            </div>
         </div>
     );
 };
-
 export default Weather;
-/*const weatherInfo = () => {
-    const [pos, setPos] = useState({lat: "", long: ""})
-    const getLocation = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(getCoords)
-        } else {
-            alert('GeoLocation not enabled');
-        }
-    }
-    const getCoords = (pos) => {
-        console.log(pos)
-        setPos({
-          lat: pos.coords.latitude,
-          long: pos.coords.longitude
-        })
-    }
-    return (
-        <div>
-            <button onClick={(event) => getLocation(event.target.value)}>Click Me</button>
-            <p>latitude: {pos.lat}</p>
-            <p>longitude: {pos.long}</p>
-        </div>
-    );
-};*/
